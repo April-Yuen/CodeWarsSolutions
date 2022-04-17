d5c96f879143a0e981c97151943fa131df271288
April Yuen <april.you@gmail.com>
2022-04-17 14:27:08 -0400

    Adding an 8kyu solution.

---
+++ removeExMarks.js
@@ -0,0 +1,8 @@
+// Remove exclmamation marks
+
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+    return s.replace(/!/g, "")
+   }
+
