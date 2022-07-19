6274c7954107644227109647cd59ada82f6232f6
April Yuen <april.you@gmail.com>
2022-07-19 09:53:35 -0400

    Adding a 7kyu solution.

---
+++ min-array.js
@@ -0,0 +1,9 @@
+// Form The Minimum
+
+// Task
+// Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
+
+function minValue(values){
+    let arr = Array.from(new Set(values))
+    return parseInt(arr.sort().join(''))
+  }
