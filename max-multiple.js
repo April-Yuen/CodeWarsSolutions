beebf1451733e5aea706ca973c864556d30ade83
April Yuen <april.you@gmail.com>
2022-11-27 21:55:10 -0500

    Adding a 7kyu solution.

---
+++ max-multiple.js
@@ -0,0 +1,28 @@
+// Maximum Multiple
+
+// DESCRIPTION:
+// Task
+// Given a Divisor and a Bound , Find the largest integer N , Such That ,
+
+// Conditions :
+// N is divisible by divisor
+
+// N is less than or equal to bound
+
+// N is greater than 0.
+
+// Notes
+// The parameters (divisor, bound) passed to the function are only positive values .
+// It's guaranteed that a divisor is Found .
+// Input >> Output Examples
+// maxMultiple (2,7) ==> return (6)
+
+function maxMultiple(divisor, bound){
+    let maxMul=[] 
+    for(let i = 0; i <= bound; i++){
+      if(i % divisor === 0){
+        maxMul.push(i)
+      }
+    }
+    return maxMul[maxMul.length-1]
+}
