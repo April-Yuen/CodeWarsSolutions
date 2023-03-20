8a1e727497ccdf12b80c77480bdeecc5cf0d960d
April Yuen <april.you@gmail.com>
2023-03-19 21:10:02 -0400

    Adding an 8kyu java solution.

---
+++ calAvgJava.java
@@ -0,0 +1,18 @@
+// Calculate average
+
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+// import java.util.*;
+
+// public class Kata{
+//   public static double find_average(int[] array){
+//     double sum = 0;
+//     for(int i = 0; i < array.length; i++){
+//       sum += array[i];
+//     }
+    
+//     return sum/ array.length;
+//   }
+// }
