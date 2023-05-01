958f0e377fe783a0d19b55b8b5bda5fa7d2e6b76
April Yuen <april.you@gmail.com>
2023-05-01 19:59:39 -0400

    Adding a 7kyu java solution.

---
+++ Spacify.java
@@ -0,0 +1,12 @@
+// Spacify
+
+// DESCRIPTION:
+// Modify the spacify function so that it returns the given string with spaces inserted between each character.
+
+// spacify("hello world") // returns "h e l l o   w o r l d"
+
+public class Spacify {
+    public static String spacify(String str){
+        return str.replace("", " ").trim(); //show me the code!
+    }
+}
