ea4a69aaef2d464372febfd423bbf7c8aa59fd6a
April Yuen <april.you@gmail.com>
2023-05-26 10:04:28 -0400

    Adding a 7kyu codewars solution.

---
+++ meetingFour.java
@@ -0,0 +1,29 @@
+// The Office IV - Find a Meeting Room
+
+// DESCRIPTION:
+// Your job at E-Corp is both boring and difficult. It isn't made any easier by the fact that everyone constantly wants to have a meeting with you, and that the meeting rooms are always taken!
+
+// In this kata, you will be given an array. Each value represents a meeting room. Your job? Find the first empty one and return its index (N.B. There may be more than one empty room in some test cases).
+
+// 'X' --> busy
+// 'O' --> empty
+// If all rooms are busy, return "None available!"
+
+// More in this series:
+
+// The Office I - Outed
+// The Office II - Boredeom Score
+// The Office III - Broken Photocopier
+// The Office V - Find a Chair
+
+public class TheOffice {
+    public static Object meeting(char[] x) {
+      
+      for(int i = 0; i < x.length; i++){
+        if(x[i] == 'O'){
+          return i;
+        }
+      }
+      return "None available!";
+    }
+  }
