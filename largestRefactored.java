de57cc2b26ff4d99d0ebc9f2a59a61f8c668db26
April Yuen <april.you@gmail.com>
2023-05-28 11:28:37 -0400

    Adding a 7kyu refactored solution.

---
+++ largestRefactored.java
@@ -0,0 +1,19 @@
+// Largest Elements
+
+// DESCRIPTION:
+// Write a program that outputs the top n elements from a list.
+
+// Example:
+
+// largest(2, new int[]{7, 6, 5, 4, 3, 2, 1})
+// // => new int[]{6, 7}
+
+import java.util.Arrays;
+
+public class Solution {
+  
+   public static int[] largest(int n, int[] arr) {
+     Arrays.sort(arr);
+     return Arrays.copyOfRange(arr, arr.length-n, arr.length);
+   }
+}
